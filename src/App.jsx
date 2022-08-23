6cd6e60b75946a49a325304499169551ab2fbc52
samiulalimsaad <saadnbiuraj@gmail.com>
2022-08-23 11:43:00 +0600

    store added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import { useState } from "react";
+import { Provider } from "react-redux";
 import Header from "./Components/Header";
 import Navbar from "./Components/Navbar";
 import TodoList from "./Components/Todo/List";
+import { store } from "./redux/store";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
-        <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
-            <Navbar />
-            <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
-                <Header />
-                <hr className="mt-4" />
-                <TodoList />
-                <hr className="mt-4" />
+        <Provider store={store}>
+            <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
+                <Navbar />
+                <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+                    <Header />
+                    <hr className="mt-4" />
+                    <TodoList />
+                    <hr className="mt-4" />
+                </div>
             </div>
-        </div>
+        </Provider>
     );
 }
 
